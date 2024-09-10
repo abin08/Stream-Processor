d2cbc86784fab54aa4bbb3c69933d22698c897f2
abantony <abin.antony@oracle.com>
2024-09-10 13:57:39 +0530

    read data from kafka

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 
-spark_master_url = 'spark://abantony-mac:7077'
-conf = SparkConf().setAppName("Access Log Analysis").setMaster(spark_master_url)
 
-spark = SparkSession.builder.config(conf=conf).getOrCreate()
+# Define schema for the log data
+log_schema = StructType(
+    [
+        StructField("client_ip", StringType()),
+        StructField("user_id", StringType()),
+        StructField("user", StringType()),
+        StructField("timestamp", TimestampType()),
+        StructField("request_method", StringType()),
+        StructField("request", StringType()),
+        StructField("http_version", StringType()),
+        StructField("response_code", IntegerType()),
+        StructField("bytes", IntegerType()),
+        StructField("referrer", StringType()),
+        StructField("user_agent", StringType()),
+    ]
+)
 
-file_name = "data/access.log"
 
-rdd = spark.sparkContext.textFile(file_name)
-first_5_lines = rdd.take(5)
+spark_master_url = "spark://abantony-mac:7077"
+conf = SparkConf().setAppName("AccessLogAnalysis").setMaster(spark_master_url)
 
-for line in first_5_lines:
-    print(line)
+# Create SparkSession
+builder = SparkSession.Builder()
+spark = builder.config(conf=conf).getOrCreate()
 
-spark.stop()
 
+# Read data from Kafka
+df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "access_logs")
+    .option("startingOffsets", "earliest")
+    .load()
+)
