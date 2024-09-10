a60c3721c5d098c57b98108a6b056f655f2e3d90
abantony <abin.antony@oracle.com>
2024-09-10 16:06:09 +0530

    parse the data

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+from pyspark.sql.functions import from_json, col
 
 
 # Define schema for the log data
 log_schema = StructType(
     [
         StructField("client_ip", StringType()),
+        StructField("rfc_id", StringType()),
         StructField("user_id", StringType()),
-        StructField("user", StringType()),
-        StructField("timestamp", TimestampType()),
-        StructField("request_method", StringType()),
+        StructField("@timestamp", TimestampType()),
+        StructField("http_method", StringType()),
         StructField("request", StringType()),
         StructField("http_version", StringType()),
         StructField("response_code", IntegerType()),
@@ -28,7 +29,6 @@ conf = SparkConf().setAppName("AccessLogAnalysis").setMaster(spark_master_url)
 builder = SparkSession.Builder()
 spark = builder.config(conf=conf).getOrCreate()
 
-
 # Read data from Kafka
 df = (
     spark.readStream.format("kafka")
@@ -37,3 +37,9 @@ df = (
     .option("startingOffsets", "earliest")
     .load()
 )
+
+# Parse JSON data using the log_schema
+parsed_df = df.select(
+    from_json(col("value").cast("string"), log_schema).alias("data")
+).select("data.*")
+
